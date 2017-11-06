5d0d23c4021b1a041beaa0a52c093334480caa78
delmarrh1567 <33227546+delmarrh1567@users.noreply.github.com>
2017-11-06 11:39:23 -0500

    Add files via upload

---
+++ M3T1_DelMarr Hello World 6 November 2017.py
@@ -0,0 +1,50 @@
+Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:01:18) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print ("Hello World")
+Hello World
+>>> 2+2
+4
+>>> 
+========= RESTART: C:/Users/delmarrh1567/Desktop/Python Practice.py =========
+Traceback (most recent call last):
+  File "C:/Users/delmarrh1567/Desktop/Python Practice.py", line 1, in <module>
+    Print("Hello World")
+NameError: name 'Print' is not defined
+>>> 
+========= RESTART: C:/Users/delmarrh1567/Desktop/Python Practice.py =========
+Traceback (most recent call last):
+  File "C:/Users/delmarrh1567/Desktop/Python Practice.py", line 1, in <module>
+    Print("Hello World")
+NameError: name 'Print' is not defined
+>>> 
+========= RESTART: C:/Users/delmarrh1567/Desktop/Python Practice.py =========
+Traceback (most recent call last):
+  File "C:/Users/delmarrh1567/Desktop/Python Practice.py", line 1, in <module>
+    Print( "Hello World" )
+NameError: name 'Print' is not defined
+>>> 
+========= RESTART: C:/Users/delmarrh1567/Desktop/Python Practice.py =========
+Traceback (most recent call last):
+  File "C:/Users/delmarrh1567/Desktop/Python Practice.py", line 7, in <module>
+    Print ("Hello World")
+NameError: name 'Print' is not defined
+>>> Print ("Hello World")
+Print ("2+2")
+Print ("3+4")
+SyntaxError: multiple statements found while compiling a single statement
+>>> Print ("Hello World")
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    Print ("Hello World")
+NameError: name 'Print' is not defined
+>>> 
+========= RESTART: C:/Users/delmarrh1567/Desktop/Python Practice.py =========
+Hello World
+2+2
+3+4
+>>> 
+=========== RESTART: C:/Users/delmarrh1567/Desktop/M3T1_DelMarr.py ===========
+Hello World
+2+2
+3+4
+>>> 
