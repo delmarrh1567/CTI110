2a0a9999b126a6e3179a1369b22967fdbf231452
delmarrh1567 <33227546+delmarrh1567@users.noreply.github.com>
2017-11-15 11:04:03 -0500

    Add files via upload

---
+++ M4T1_SalePrediction_DelMarr.py
@@ -0,0 +1,16 @@
+#CTI-110
+#M4T1 - Sales Prediction
+#Hasan DelMarr
+#13 November 2017
+#
+# Get the projected total sales.
+total_sales = float(input("Enter the projected sales:"))
+
+#Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print ("The profit is $", format (profit, ",.2f"))
+
+
+
