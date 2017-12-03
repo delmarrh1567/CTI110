b2b64f5fa2fa975b5c5da09f8c209cdb8171216a
delmarrh1567 <33227546+delmarrh1567@users.noreply.github.com>
2017-12-03 18:24:36 -0500

    Add files via upload

---
+++ M6HW2_DelMarr.py
@@ -0,0 +1,14 @@
+#CTI-110
+#M6HW2 - Running Total
+#Hasan DelMarr
+#1 December 2017
+
+#Defining Running Total Variables
+
+total = 0
+for i in range(5):
+    currentnumber = int (input("Enter a Number:"))
+    total += currentnumber
+print("The total is: ",total)
+      
+    
